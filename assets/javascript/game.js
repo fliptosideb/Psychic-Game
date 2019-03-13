08750ac3fa1e9c61d538e7acf8f386f793d86b79
Jonathan Lui <jonathanlui@dhcp-v105-078.mobile.uci.edu>
2019-03-12 17:57:57 -0700

    done?

---
+++ assets/javascript/game.js
@@ -0,0 +1,31 @@
+        var comChoice = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        let wins = 0;
+        let losses = 0;
+        let tries = 9;
+
+        document.onkeyup = e => {
+            var userGuess = e.key;
+            if (e.keyCode <= 64 || e.keyCode >= 91) {
+                alert('Please guess a letter')
+                return null
+            } else {
+                document.querySelector('#user').innerHTML = userGuess;
+            }
+            var comGuess = comChoice[Math.floor(Math.random() * comChoice.length)];
+            document.querySelector('#com').innerHTML = comGuess;
+            if (userGuess === comGuess) {
+                wins++;
+                alert('You win!');
+                document.querySelector('#wins').innerHTML = wins;
+                tries = 9;
+            } else if (tries === 0) {
+                losses++;
+                alert('You are a loser!');
+                document.querySelector('#losses').innerHTML = losses;
+                tries = 9;
+                document.querySelector('#left').innerHTML = tries;
+            } else {
+                alert('Wrong!')
+                tries--;
+                document.querySelector('#left').innerHTML = tries;
+            }}
